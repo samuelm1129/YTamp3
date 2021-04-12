26678da674694a9bd3c09f76b48b266df61ed80b
Samuel Mesa <78155946+samuelm1129@users.noreply.github.com>
2021-04-12 08:00:15 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import youtube_dl
+
+#Se pide el link
+input_url = input("Ingresa el link del video que deseas convertir: ")
+
+#info   
+video_info = youtube_dl.YoutubeDL().extract_info(url=input_url, download=False)
+video_title = video_info['title']
+
+#config
+opciones = {
+    'format': 'bestaudio/best',
+    'outtmpl': f"C:/Users/Usuario/Downloads/musica/{video_title}.mp3",
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }]
+}
+
+#descarga
+with youtube_dl.YoutubeDL(opciones) as ydl:
+    ydl.download([input_url])
